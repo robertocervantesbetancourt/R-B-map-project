e7b63edd24667e26d98ced05e04dc237846dd9b6
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-08-17 21:29:53 +0000

    Fix users schema file

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -4,5 +4,5 @@ DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
-  profile_photo VARCHAR(255),
+  profile_photo VARCHAR(255)
 );
