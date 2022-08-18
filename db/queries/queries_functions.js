b6d36a78bbc9160bb76fdbdd63a256abe4d3570b
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-08-18 23:21:32 +0000

    change get user/:id route to include all data

--- db/queries/queries_functions.js
+++ db/queries/queries_functions.js
@@ -1,10 +1,10 @@
 const userById = (db, id) => {
   return db.query(
-    `SELECT users_name, profile_photo, map_name, maps.map_id, location_id, location_name,
-    location_image, location_ description, location_latitude, location_longitude
-    FROM users JOIN maps ON users.id = maps.creator_id
-    JOIN locations ON users.id = locations.creator_id
-    WHERE users.id = $1;`, [id]);
+    `SELECT user_name, profile_photo, map_name, maps.map_id, location_id, location_name,
+    location_image, location_description, location_latitude, location_longitude
+    FROM users JOIN maps ON users.user_id = maps.creator_id
+    JOIN locations ON users.user_id = locations.creator_id
+    WHERE users.user_id = $1;`, [id]);
 };
 
 const mapsFromOtherUsers = (db, id) => {
