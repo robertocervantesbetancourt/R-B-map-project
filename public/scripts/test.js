4232e18dbb57ff1fced57208479ee2318fa38f96
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-08-26 23:02:00 +0000

    COnnect front end and back end

--- public/scripts/test.js
+++ public/scripts/test.js
@@ -1,92 +1,33 @@
-$(document).ready(function(){
+// const { on } = require("nodemon");
 
-  //Get route when clicking on a map or a location
-  $(".map-button input").on('click', function(e){
-    let mapId = $(this).attr("map-id");
-    console.log('The map id is ' + mapId);
-    e.preventDefault()
-    $.ajax({
-      url: `/map/${mapId}`,
-      type: 'get',
-    })
-    .then(function(data){
-      console.log('ajax call...')
-      $('#locations-list').html(data)
-    })
-    .then(function(){
-      $(".location-button input").on('click', function(e){
-        let locId = $(this).attr('location-id');
-        console.log('The location id is ' + locId);
-        e.preventDefault()
-        $.ajax({
-          url: `/location/${locId}`,
-          type: 'get',
-        })
-        .then(function(data){
-          console.log('ajax call...')
-          $('#location-info').html(data)
-        })
-      })
-    })
-  })
+//Google map
+function initMap() {
 
-  //POST route when creating a new map
-  const $newMap = document.querySelector('#new-map')
-  $($newMap).submit(function(e){
-    e.preventDefault();
-    const message = $(this).serialize();
-    let userID = $(this).attr("user-id")
-    $.ajax({
-      url: `/map`,
-      type: 'post',
-      data: message
-    })
-    .then(function(data){
-      $newMap.reset();
-      $('#test-map-widget').load(location.href + " #test-map-widget");
-    })
-  })
+  var myHouse = new google.maps.LatLng(43.66016319226872, -79.488492191761);
 
-    //POST route when creating a new location
-    $(".map-button input").on('click', function(){
-      const $newLocation = document.querySelector('#new-location')
-      $($newLocation).submit(function(e){
-        console.log('ajax call...')
-        e.preventDefault();
-        const message = $(this).serialize();
-        $.ajax({
-          url: `/location`,
-          type: 'post',
-          data: message
-        })
-        .then(function(data){
-          $newLocation.reset();
-          //console.log($('#location-widget').html());
-          $('#locations-list').load(location.href + " #locations-list");
-        })
-      })
-    })
+  map = new google.maps.Map(
+    document.getElementById('map'), {center: myHouse, zoom: 15});
 
-      //POST route to delete locations
-      $(".map-button input").on('click', function(){
-      const $deleteLocation = document.querySelector('#delete-location input')
-      if ($($deleteLocation).length > 0){
-        alert ('it exists')
-      }
-      $($deleteLocation).on('click', function(e){
-        console.log('ajax cal...')
-        e.preventDefault();
-        let locationID = $(this).attr('#location-id')
-        alert (`deleting ${locationID}`)
-        const message = $(this).serialize();
-        $.ajax({
-          url: `/location/${locationID}/delete`,
-          type: 'delete',
-          data: message
-        })
-        .then(function(data){
+    map.addListener("click", (e) => {
+      console.log(e)
+      coords = e.LatLng
+      placeMarkerAndPanTo(e.latLng, map);
+      $(document).ready(function() {
 
-        })
-      })
-    })
-})
+        // $coords = coords[0];
+        // console.log(coords)
+        // $('#latlong-container').append($coords);
+
+      });
+    });
+  }
+
+  function placeMarkerAndPanTo(latLng, map) {
+    new google.maps.Marker({
+      position: latLng,
+      map: map,
+    });
+    map.panTo(latLng);
+  }
+
+
--- routes/test_routes.js
+++ routes/test_routes.js
@@ -1,4 +1,16 @@
-const {userProfileById, mapsFromOtherUsers, allLocationsInMap, locationInfo, newMap, userMaps, newLocation, firstMapFromUser, deleteLocation, locationLike} = require('../db/queries/queries_functions');
+const {
+  userProfileById,
+  mapsFromOtherUsers,
+  allLocationsInMap,
+  locationInfo,
+  newMap,
+  userMaps,
+  newLocation,
+  firstMapFromUser,
+  deleteLocation,
+  locationLike,
+  firstLocationInMap} = require('../db/queries/queries_functions');
+
 const bodyParser = require('body-parser')
 const cookieParser = require ('cookie-parser')
 
@@ -9,54 +21,75 @@ module.exports = (db) => {
 
   ////////////////// GET ROUTES //////////////////
 
-  // router.get('/:id', (req, res) => {
-  //   res.render('index')
-  // })
 
-  router.get("/:id", (req, res) => {
-    userProfileById(db, req.params.id)
-      .then(data => {
-        const userID = data.rows[0].user_id;
-        const user = data.rows[0];
-        const templateVars = {user};
-        userMaps(db, req.params.id)
-        .then(data => {
-          const userMaps = data.rows;
-          templateVars.userMaps = userMaps
-          mapsFromOtherUsers(db, req.params.id)
-            .then(data => {
-              const otherMaps = data.rows;
-              templateVars.otherMaps = otherMaps
-              firstMapFromUser(db, req.params.id)
-              .then(data => {
-                const mapID = data.rows[0].map_id
-                if (userID === req.cookies.userID){
-                  allLocationsInMap(db, req.cookies.mapID)
-                  .then(data => {
-                    const locations = data.rows;
-                    templateVars.locations = locations;
-                    res
-                    .cookie('userID', `${req.params.id}`)
-                    .cookie('mapID', mapID)
-                    .render("index", templateVars);
-                  })
-                }
-                allLocationsInMap(db, mapID)
-                .then(data => {
-                  const locations = data.rows;
-                  templateVars.locations = locations;
+router.get("/user/:id", (req, res) => {
+
+    //variables setup
+    let userID = req.params.id;
+    let mapID = null;
+    let templateVars = {};
+
+    //add userID cookie
+    res.cookie('userID', userID)
+    console.log('get user/id route')
+
+
+    //If there is already a mapID cookie and use it to exract all locations from that map
+    if (req.cookies.mapID){
+      console.log('there is a cookie');
+      mapID = req.cookies.mapID;
+      res.cookie('mapID', mapID)
+      allLocationsInMap(db, mapID)
+      .then((data) => {
+        let locations = data.rows;
+        templateVars.locations = locations
+        userProfileById(db, userID)
+        .then((data) => {
+          let user = data.rows[0];
+          templateVars.user = user;
+          firstLocationInMap(db, mapID)
+          .then ((data) => {
+            let locationID = data && data.rows.length > 0 ? data.rows[0].location_id: '';
+            userMaps(db, userID)
+            .then((data) => {
+              let userMaps = data.rows;
+              templateVars.userMaps = userMaps;
+              mapsFromOtherUsers(db, userID)
+              .then((data) => {
+                let otherMaps = data.rows;
+                templateVars.otherMaps = otherMaps;
+                locationInfo(db, locationID)
+                .then((data) => {
+                  let location = data.rows[0];
+                  templateVars.location = location;
+                  //console.log(templateVars);
                   res
-                  .cookie('userID', `${req.params.id}`)
-                  .cookie('mapID', mapID)
-                  .render("index", templateVars);
+                  .clearCookie('mapID')
+                  .render('index', templateVars)
                 })
+                .catch(err => {
+                  res
+                    .status(500)
+                    .json({ error: err.message });
+                });
               })
+              .catch(err => {
+                res
+                  .status(500)
+                  .json({ error: err.message });
+              });
             })
             .catch(err => {
               res
                 .status(500)
                 .json({ error: err.message });
             });
+          })
+          .catch(err => {
+            res
+              .status(500)
+              .json({ error: err.message });
+          });
         })
         .catch(err => {
           res
@@ -69,30 +102,111 @@ module.exports = (db) => {
           .status(500)
           .json({ error: err.message });
       });
-  });
-
-  router.get("/map/:id", (req, res) => {
-    allLocationsInMap(db, req.params.id)
-      .then(data => {
-        const locations = data.rows;
-        const templateVars = {locations};
-        res
-          .cookie('mapID', `${req.params.id}`)
-          .render("test_locations", templateVars)
+    } else {
+      console.log('there is no cookie');
+      firstMapFromUser(db, userID)
+      .then (data => {
+        mapID = data.rows[0].map_id
+        res.cookie('mapID', mapID)
+        //console.log(mapID)
+        allLocationsInMap(db, mapID)
+        .then((data) => {
+          let locations = data.rows;
+          templateVars.locations = locations
+          //console.log(templateVars)
+          userProfileById(db, userID)
+          .then((data) => {
+            let user = data.rows[0];
+            templateVars.user = user;
+            firstLocationInMap(db, mapID)
+            .then ((data) => {
+              let locationID = data.rows[0].location_id;
+              userMaps(db, userID)
+              .then((data) => {
+              let userMaps = data.rows;
+              templateVars.userMaps = userMaps;
+              mapsFromOtherUsers(db, userID)
+              .then((data) => {
+                let otherMaps = data.rows;
+                templateVars.otherMaps = otherMaps;
+                locationInfo(db, locationID)
+                .then((data) => {
+                  let location = data.rows[0];
+                  templateVars.location = location;
+                  //console.log(templateVars);
+                  res
+                  .clearCookie('mapID')
+                  .render('index', templateVars)
+                })
+                .catch(err => {
+                  res
+                    .status(500)
+                    .json({ error: err.message });
+                });
+              })
+              .catch(err => {
+                res
+                  .status(500)
+                  .json({ error: err.message });
+              });
+            })
+            .catch(err => {
+              res
+                .status(500)
+                .json({ error: err.message });
+            });
+          })
+          .catch(err => {
+            res
+              .status(500)
+              .json({ error: err.message });
+          });
+        })
+        .catch(err => {
+          res
+            .status(500)
+            .json({ error: err.message });
+        });
       })
       .catch(err => {
         res
           .status(500)
-          //.json({ error: err.message });
+          .json({ error: err.message });
       });
-  });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+  }
+})
 
-  router.get("/location/:id", (req, res) => {
-    locationInfo(db, req.params.id)
+  // router.get("/location", (req, res) => {
+  //   allLocationsInMap(db, req.cookies.mapID)
+  //   .then(data => {
+
+  //   })
+  // })
+
+  router.get("/map", (req, res) => {
+    userMaps(db, req.cookies.userID)
+    .then(data => {
+      const userMaps = data.rows;
+      const templateVars = {userMaps}
+      res.render("4_map_widget", templateVars)
+    })
+  })
+
+  router.get("/map/:id", (req, res) => {
+    allLocationsInMap(db, req.params.id)
       .then(data => {
-        const location = data.rows[0];
-        const templateVars = {location};
-        res.render("test_loc_info", templateVars)
+        console.log('you are here')
+        const locations = data.rows;
+        const templateVars = {locations};
+        res
+          .cookie('mapID', `${req.params.id}`)
+          .render('8_locations_form', templateVars)
       })
       .catch(err => {
         res
@@ -101,10 +215,27 @@ module.exports = (db) => {
       });
   });
 
+  router.get("/location/:id", (req, res) => {
+    locationInfo(db, req.params.id)
+      .then(data => {
+        const location = data.rows[0];
+        const templateVars = {location};
+        res.render("9_location_info", templateVars)
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+
+
   //////////////////// POST ROUTES ///////////////////////////
 
   router.post("/map", (req, res) => {
     const userID = req.cookies.userID
+    console.log(userID)
     newMap(db, req.body.map_name,userID)
       .then(data => {
           res.status(201).send();
